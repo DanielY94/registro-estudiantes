d2e6685ee65311419695bf9528ca12f89f875b2c
Daniel yugcha <jeremias94d@gmail.com>
2026-08-06 17:25:19 -0500

    Agrega el ingreso de datos

--- main.cpp
+++ main.cpp
@@ -1,2 +1,8 @@
 #include <iostream>`nusing namespace std;`n`nint main() {`n    cout << "Sistema de Registro de Estudiantes" << endl;`n    return 0;`n}
 // Modulo para validar la cedula del estudiante
+
+// Modulo: Ingreso de datos del estudiante
+void ingresarEstudiante() {
+    string nombre;
+    cout << "Ingrese nombre: " ;
+}
